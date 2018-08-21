39dc0690ead2124c13d2346fbb155aac078a4575
Engramar Bollas <engramar.bollas@gmail.com>
2018-08-21 17:28:19 +1000

    Add files via upload

---
+++ trainer.py
@@ -0,0 +1,21 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+
+chatbot = ChatBot(
+    'Ron Obvious',
+    trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
+)
+
+chatterbot = ChatBot("My Personal Assistant",storage_adapter="chatterbot.storage.SQLStorageAdapter")
+chatterbot.set_trainer(ListTrainer)
+
+chatterbot.train([
+    "Hi!",
+    "Hello!",
+])
+
+# Quick Testing
+print("Hi!")
+response = chatterbot.get_response("Hi!")
+print(response)
